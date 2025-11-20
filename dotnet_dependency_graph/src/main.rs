0cd721fae2a3d9e9dc13c6269bb2baa616273519
Marcin Szopa <marcinsz.pv@gmail.com>
2025-11-20 20:46:52 +0100

    feat: implement basic egui application for Dotnet Dependency Viewer

--- dotnet_dependency_graph/src/main.rs
+++ dotnet_dependency_graph/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::{App, run_native};
+use egui::Context;
+
+#[derive(Default)]
+struct DependencyApp;
+
+impl App for DependencyApp {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+    run_native(
+        "Dotnet Dependency Viewer",
+        native_options,
+        Box::new(move |_| Ok(Box::<DependencyApp>::default())),
+    )
 }
