db45374c979246cceb76b21e82a3666b12530fdb
Marcin Szopa <marcinsz.pv@gmail.com>
2025-11-20 21:03:35 +0100

    feat: implement file dialog

--- dotnet_dependency_graph/src/main.rs
+++ dotnet_dependency_graph/src/main.rs
@@ -1,14 +1,57 @@
 use eframe::{App, run_native};
 use egui::Context;
+use egui_file_dialog::FileDialog;
+use std::path::PathBuf;
 
-#[derive(Default)]
-struct DependencyApp;
+struct File {
+    path: PathBuf,
+}
+
+impl File {
+    fn new(path: PathBuf) -> Self {
+        Self { path }
+    }
+}
+
+struct DependencyApp {
+    file_dialog: FileDialog,
+    current_dgspec_file: Option<File>,
+}
+
+impl DependencyApp {
+    fn new() -> Self {
+        Self {
+            file_dialog: FileDialog::new(),
+            current_dgspec_file: None,
+        }
+    }
+}
 
 impl App for DependencyApp {
     fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
+        // --- Menu Bar using Top Panel ---
+        egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Open file").clicked() {
+                        self.file_dialog.pick_file();
+                    }
+                });
+            });
         });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some(file) = &self.current_dgspec_file {
+                ui.label(format!("Picked file: {:?}", &file.path));
+            } else {
+                ui.label("Choose the file.");
+            }
+        });
+
+        self.file_dialog.update(ctx);
+        if let Some(path) = self.file_dialog.take_picked() {
+            self.current_dgspec_file = Some(File::new(path.to_path_buf()));
+        }
     }
 }
 
@@ -17,6 +60,6 @@ fn main() -> Result<(), eframe::Error> {
     run_native(
         "Dotnet Dependency Viewer",
         native_options,
-        Box::new(move |_| Ok(Box::<DependencyApp>::default())),
+        Box::new(move |_| Ok(Box::new(DependencyApp::new()))),
     )
 }
