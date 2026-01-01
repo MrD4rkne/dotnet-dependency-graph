328846b13dc3bf16c9a4a8e05ac8808122b7131c
Marcin Szopa <marcinsz.pv@gmail.com>
2026-01-01 15:54:12 +0100

    refactor: update NativeOptions to include viewport settings and WGPU renderer

--- dotnet_dependency_graph/src/main.rs
+++ dotnet_dependency_graph/src/main.rs
@@ -16,10 +16,20 @@ use crate::app::DependencyApp;
 fn main() -> std::result::Result<(), eframe::Error> {
     let server = maybe_start_puffin_server();
 
-    let native_options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 1024.0])
+            .with_drag_and_drop(true),
+
+        #[cfg(feature = "wgpu")]
+        renderer: eframe::Renderer::Wgpu,
+
+        ..Default::default()
+    };
+
     let result = run_native(
         "Dotnet Dependency Viewer",
-        native_options,
+        options,
         Box::new(move |_| Ok(Box::new(DependencyApp::default()))),
     );
 
