e9a17df394a5441fd48982741f1071d2e6e03220
Marcin Szopa <marcinsz.pv@gmail.com>
2026-01-04 13:42:47 +0100

    refactor: introduce layout configuration and integrate with file dialog handling

---
+++ dotnet_dependency_graph/src/layout_options.rs
@@ -0,0 +1,150 @@
+use eframe::egui::{ComboBox, Context, Slider, Window};
+
+#[derive(Clone, Debug, PartialEq, Copy)]
+enum RankingType {
+    ForceUp,
+    ForceDown,
+}
+
+impl Into<dotnet_dependency_parser::graph::algo::RankingType> for RankingType {
+    fn into(self) -> dotnet_dependency_parser::graph::algo::RankingType {
+        match self {
+            RankingType::ForceUp => dotnet_dependency_parser::graph::algo::RankingType::Up,
+            RankingType::ForceDown => dotnet_dependency_parser::graph::algo::RankingType::Down,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Copy)]
+enum CrossingMinimization {
+    Median,
+    Barycenter,
+}
+
+impl Into<dotnet_dependency_parser::graph::algo::CrossingMinimization> for CrossingMinimization {
+    fn into(self) -> dotnet_dependency_parser::graph::algo::CrossingMinimization {
+        match self {
+            CrossingMinimization::Median => {
+                dotnet_dependency_parser::graph::algo::CrossingMinimization::Median
+            }
+            CrossingMinimization::Barycenter => {
+                dotnet_dependency_parser::graph::algo::CrossingMinimization::Barycenter
+            }
+        }
+    }
+}
+
+/// Configuration for layout algorithm.
+#[derive(Clone, Debug, PartialEq, Copy)]
+pub struct LayoutConfig {
+    /// spacing between nodes in the same layer
+    node_spacing: f64,
+    /// spacing between layers
+    layer_spacing: u32,
+    // how nodes are ranked and ditributed
+    ranking_type: RankingType,
+    // Which heuristic to use when minimizing edge crossings.
+    c_minimization: CrossingMinimization,
+    // whether to try to minimalize crosses of the edges by swapping nodes in the same layer.
+    minimalize_crosses: bool,
+}
+
+impl Default for LayoutConfig {
+    fn default() -> Self {
+        Self {
+            node_spacing: 1.0,
+            layer_spacing: 10,
+            ranking_type: RankingType::ForceDown,
+            c_minimization: CrossingMinimization::Barycenter,
+            minimalize_crosses: true,
+        }
+    }
+}
+
+impl From<LayoutConfig> for dotnet_dependency_parser::graph::algo::Config {
+    fn from(layout_config: LayoutConfig) -> Self {
+        dotnet_dependency_parser::graph::algo::Config {
+            minimum_length: layout_config.layer_spacing,
+            vertex_spacing: layout_config.node_spacing,
+            ranking_type: layout_config.ranking_type.into(),
+            dummy_vertices: true,
+            dummy_size: Default::default(),
+            c_minimization: layout_config.c_minimization.into(),
+            transpose: layout_config.minimalize_crosses,
+        }
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct LayoutWindow {
+    visible: bool,
+    config: LayoutConfig,
+}
+
+impl LayoutWindow {
+    pub(crate) fn update(&mut self, ctx: &Context) {
+        Window::new("Layout Configuration")
+            .open(&mut self.visible)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.label("Node spacing");
+                ui.add(Slider::new(&mut self.config.node_spacing, 10.0..=20.0));
+
+                ui.label("Layer spacing");
+                ui.add(Slider::new(&mut self.config.layer_spacing, 10..=40));
+
+                ui.horizontal(|ui| {
+                    ui.label("Ranking method:");
+                    ComboBox::from_id_salt("ranking_method")
+                        .selected_text(format!("{:?}", self.config.ranking_type))
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.config.ranking_type,
+                                RankingType::ForceUp,
+                                "Force nodes up",
+                            );
+                            ui.selectable_value(
+                                &mut self.config.ranking_type,
+                                RankingType::ForceDown,
+                                "Force nodes down",
+                            );
+                        });
+                });
+
+                ui.checkbox(
+                    &mut self.config.minimalize_crosses,
+                    "Try to minimalize edge crossing",
+                );
+
+                ui.horizontal(|ui| {
+                    ui.label("Crossing edges minimalization method:");
+                    ComboBox::from_id_salt("crosssing_edges_minimalizing_method")
+                        .selected_text(format!("{:?}", self.config.c_minimization))
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.config.c_minimization,
+                                CrossingMinimization::Median,
+                                "Median",
+                            );
+                            ui.selectable_value(
+                                &mut self.config.c_minimization,
+                                CrossingMinimization::Barycenter,
+                                "Barycenter",
+                            );
+                        });
+                });
+
+                if ui.button("Reset to defaults").clicked() {
+                    self.config = LayoutConfig::default();
+                }
+            });
+    }
+
+    pub(crate) fn request_show(&mut self) {
+        self.visible = true;
+    }
+
+    pub(crate) fn get_config(&self) -> LayoutConfig {
+        self.config
+    }
+}
